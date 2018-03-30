6fa108549a98180e93577b40ee3621443ad58765
jang <jang_inhwa@microad.co.jp>
2018-03-30 12:28:43 +0900

    Add branches

--- fetch_rebase.sh
+++ fetch_rebase.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/bash
 
-#1行明は使用するシェル
-#To execute . fetch_rebase.sh 
-#変数宣言スペース入れない
+#To execute
+#. fetch_rebase.sh 
 
 PY_B="PythonGround"
 JV_B="PlayGround"
 SQ_B="ScalaQuiz"
+SC_B="ScalaGround"
+JS_B="JavaScriptGround"
+BS_B="ba-sh"
 
 BRANCHES=(
 ${PY_B} 
 ${JV_B} 
 ${SQ_B} 
+${SC_B} 
+${JS_B} 
+${BS_B}
 )
 
 echo $LF
