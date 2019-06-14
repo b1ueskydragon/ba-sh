0a726a405063f3a473b378e1ee21e6679d718d8d
jang <dragoalie@gmail.com>
2019-06-14 16:53:13 +0900

    touppercase

--- leetcode/p0176/solve.sql
+++ leetcode/p0176/solve.sql
@@ -1,7 +1,7 @@
 SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM employee);
 
-SELECT IFNULL((SELECT DISTINCT Salary FROM Employee order by Salary desc limit 1, 1), null) AS SecondHighestSalary;
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1, 1), null) AS SecondHighestSalary;
 
-SELECT (SELECT DISTINCT Salary FROM Employee order by Salary desc limit 1, 1) AS SecondHighestSalary;
+SELECT (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1, 1) AS SecondHighestSalary;
 
 SELECT MAX(Salary) AS SecondHighestSalary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee);
