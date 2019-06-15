9168bb55d7f25c0e456f85d75c1158c0a26028b2
jang <dragoalie@gmail.com>
2019-06-15 14:01:58 +0900

    fix comment

--- leetcode/p0192/solve.sh
+++ leetcode/p0192/solve.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 # Read from the file words.txt and output the word frequency list to stdout.
-
-# awk 1 add a line in the end of file if line does not exist
-#cat words.txt | awk 1 | while read line
-#do
-#  echo ${line} | tr ' ' '\n'
-#done
-
+# make an associative array first
 awk '{for(i=1;i<=NF;i++) dict[$i]++} END {for(word in dict) print word, dict[word]}' words.txt | sort -k 2 -rn
